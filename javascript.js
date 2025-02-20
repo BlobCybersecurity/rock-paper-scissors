15967c4527aa14db99c04eda3af870800a1efd0c
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2025-02-20 16:06:51 +0100

    Add gethuman function and variables humscore computerscore

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 // Create a new function getComputerChoice
 
+// initialize new variables humanScore and computerScore with 0
+
+let humanScore = 0;
+let computerScore = 0;
+
+ 
 function getComputerChoice() {
     
     
     
     
-    let result = Math.floor(Math.random() * 3)
+    let result = Math.floor(Math.random() * 3) // result be a random number, 0, 1 or 2
 
-    if (result === 0) {
+    if (result === 0) { // if result is 0 print this if 1 print this or if 2 pring that
         console.log("rock")
     } else if (result === 1) {
         console.log("paper")
@@ -21,8 +27,46 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
+ // call the function
 
-// Assign each of these: "rock", "paper", "scissors" a random number between 0 and 1 
+// Create a new function getHumanChoice 
 
-// Use math.random to return one of these random strings as the getcomputer function choice
+// prompt the user
+
+function getHumanChoice() {
+    let question = prompt("What would you like to choose? Rock Paper or Scissors: ")
+
+// Ask in a prompt wether the user chooses rock, paper or scissors
+        // use an if statement
+
+// make the function return what the user said 
+
+
+    if (question === "rock") {
+        return "rock"
+    } else if (question === "paper") {
+        return "paper"
+
+    } else if (question === "scissors") {
+        return "scissors"
+    } else {
+        return "Invalid value, please try again."
+    }
+}
+
+
+
+
+
+// logic to play a round
+
+function playRound() {
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+
